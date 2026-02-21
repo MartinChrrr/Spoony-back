21c942de31a8bb30ee437bcc1eb3360d146eebe1
martin <martin_charrier@yahoo.fr>
2026-02-21 23:25:13 +0100

    init backend

---
+++ src/main/java/com/spoony/backend/SpoonyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spoony.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoonyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoonyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spoony/backend/SpoonyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spoony.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoonyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
